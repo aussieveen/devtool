154689b81c4295bdf559ff53cfb49762658ef73f
Simon McWhinnie <simon.mcwhinnie@gmail.com>
2026-04-13 12:55:43 +0100

    fix: resolve conflict

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
     let config_loader = ConfigLoader::new(".devtool", "config.yaml");
-    let config = config_loader.read_config()?;
+    let config = config_loader.read_or_create_config()?;
     let result = App::new(config, config_loader).run(terminal).await;
     ratatui::restore();
     result
